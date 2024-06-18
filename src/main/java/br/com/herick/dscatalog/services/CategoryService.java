cc207959e066eb655f18e6c575c184c094b0adac
Herick alves pereira <42222113+herickkgb@users.noreply.github.com>
2024-06-18 20:10:18 -0300

    inserindo dados DataBase

--- src/main/java/br/com/herick/dscatalog/services/CategoryService.java
+++ src/main/java/br/com/herick/dscatalog/services/CategoryService.java
@@ -4,16 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import br.com.herick.dscatalog.entities.Category;
 import br.com.herick.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
-	public List<Category> findAll(){
+
+	@Transactional(readOnly = true)
+	public List<Category> findAll() {
 		return categoryRepository.findAll();
 	}
 }
