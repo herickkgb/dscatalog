ad5647757a46824fbfd853fede4f37fd303d6d2f
Herick alves pereira <42222113+herickkgb@users.noreply.github.com>
2024-06-24 17:49:04 -0300

    implementando o @BeforeEach

---
+++ src/test/java/br/com/herick/dscatalog/FixturesTests.java
@@ -0,0 +1,32 @@
+package br.com.herick.dscatalog;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FixturesTests {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		// TODO
+	}
+
+}
--- src/test/java/br/com/herick/dscatalog/repositories/ProductRepositoryTests.java
+++ src/test/java/br/com/herick/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,29 +3,39 @@ package br.com.herick.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import br.com.herick.dscatalog.entities.Product;
 
 @DataJpaTest
 public class ProductRepositoryTests {
 
+	private Long existingId;
+	private Long noexistingId;
+
 	@Autowired
 	private ProductRepository repository;
 
+	@BeforeEach
+	void setUp() throws Exception {
+		this.existingId = 1L;
+		this.noexistingId = 1000L;
+	}
+
 	@Test
-    public void deleteShouldDeleteObjectWhenIdExists() {
-        // Arrange
-        Long existingId = 1L;
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		// Arrange
 
-        // Act
-        repository.deleteById(existingId);
-        Optional<Product> result = this.repository.findById(existingId);
+		// Act
+		repository.deleteById(this.existingId);
+		Optional<Product> result = this.repository.findById(this.existingId);
 
-        // Assert
-        Assertions.assertFalse(result.isPresent());
-    }
+		// Assert
+		Assertions.assertFalse(result.isPresent());
+	}
+
 }
